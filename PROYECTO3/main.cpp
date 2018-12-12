8a586c6b5120e45fcad544ad66ab310898fa1d53
DorisAlejandra <alejita13102015@gmail.com>
2018-12-12 10:51:28 -0500

    ejercicios adicionales

---
+++ PROYECTO3/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+// obtener el cuadrado y el cubo de un numero ingresado por el usuario
+
+using namespace std;
+int cuadrado = 0;
+int resultado ;
+
+int  obtenerCuadrado(int a){
+    resultado =a*a;
+    return resultado;
+}
+int  obtenerCubo(int b){
+    resultado = b*b*b;
+   return resultado;
+}
+
+int main()
+{
+    double numero = 0;
+    int opcion = 0;
+    int resultado = 0;
+
+    cout << "Ingrese un numero para operar" << endl;
+    cin >> numero;
+    cout << "Ingrese una opcion 1.el cuadrado y 2.el cubo de un numero " << endl;
+    cin >> opcion;
+    if (opcion == 1){
+        resultado = obtenerCuadrado(numero);
+        cout << "El cuadrado del numero " << numero << "es :" << resultado << endl;
+    }else{
+        if (opcion == 2){
+            resultado = obtenerCubo( numero );
+            cout << "El cubo del numero " << numero << "es :" << resultado << endl;
+        }else{
+            cout << "Opción incorrecta " <<endl;
+        }
+    }
+
+    return 0;
+}
